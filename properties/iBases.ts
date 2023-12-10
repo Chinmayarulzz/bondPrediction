537d69902b298b0725a3fa28e772c89a8ab5b119
chinmaya.patel <chinmaya.patel@skyspecs.com>
2023-12-10 14:44:32 +0530

    Updated orbital class

---
+++ properties/iBases.ts
@@ -0,0 +1,15 @@
+class OrbitalStrength{
+
+    private strength: number;
+    private atomicNumber: number;
+
+    public OrbitalStrength(atomicNumber: number){
+        this.atomicNumber = atomicNumber;
+    }
+
+    getOrbitalDistribution(): String{
+        
+        return "";
+    }
+
+}
